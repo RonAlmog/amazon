603ac36970a1226ddf1c416b786ae2556cf2cf75
Ron Almog <ron8132@gmail.com>
2023-03-30 10:33:28 -0400

    add gradient on homepage

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -7,6 +7,7 @@ type Props = {};
 const Banner = (props: Props) => {
   return (
     <div className="relative">
+      <div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20" />
       <Carousel
         autoPlay
         infiniteLoop
