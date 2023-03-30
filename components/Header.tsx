a9edb02556df86bf7e421c9c0d560d6042b8fd00
Ron Almog <ron8132@gmail.com>
2023-03-30 09:28:51 -0400

    add responsive horizontal menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -53,7 +53,22 @@ const Header = (props: Props) => {
           </div>
         </div>
       </div>
-      <div></div>
+      {/* bottom nav */}
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+        <p className="link flex items-center">
+          <Bars3Icon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today&apos;s Deals</p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food & Groceries</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+      </div>
     </header>
   );
 };
