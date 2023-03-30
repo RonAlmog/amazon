b123c4c95f6260e70e5af79c453355b3a2c7db74
Ron Almog <ron8132@gmail.com>
2023-03-30 07:57:24 -0400

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,19 @@ module.exports = {
     // Or if using `src` directory:
     // "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        amazon_blue: {
+          light: "#232F3E",
+          DEFAULT: "#131921",
+        },
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
